4510ad41b57759d0641a72a151a99298c4b9710b
Taimoor Ali <taimoor0217@gmail.com>
2020-05-23 04:16:11 +0500

    create and read operation

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 const db = require('./dbConfig')
 const Book = require('./models/book')
-app.use(express.json());
 app.set('views' , path.join(__dirname , 'views'));
 app.set(`view engine` , 'pug')
 app.use('/public', express.static(__dirname + '/public'));
-  
+app.use(bodyParser.urlencoded({ extended: true }));
 db
   .authenticate()
   .then(() => {
@@ -18,15 +18,6 @@ db
     console.error('Unable to connect to the database:', err);
   });
   
-  
-  
-  app.get('/ping' , (req , res)=>{
-    res.send("pong")
-  })
-
-  app.get('/' , (req , res)=>{
-      res.redirect('/books')
-  })
   app.get('/books' , (req , res)=>{
     Book.findAll().then(data => {
         res.render("all_books" , {
@@ -34,13 +25,97 @@ db
         })
     });  
   })
-
+  app.get('/' , (req , res)=>{
+      res.redirect('/books')
+  })
+  app.get('/books/new' , (req , res)=>{
+    res.render("new_book" , {
+      "error": false,
+      "fields": []
+    })
+  })
   app.get('/books/:id' , (req , res)=>{
-    res.render("book_detail")
+    book = Book.findAll({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(data=>{
+      // console.log(data[0].dataValues)
+      res.render("book_detail" , {
+        ...data[0].dataValues,
+        "error": false,
+        "fields": []
+      })
+    })
+    .catch(err=>{
+      res.render("page_not_found")
+    })
   })
 
-  app.get('/new' , (req , res)=>{
-    res.render("new_book")
+
+  app.post('/books/new' , (req ,res)=>{
+    let error = false;
+    let missing = [];
+    data = req.body
+    if (!data.title){
+      error = true
+      missing.push("title")
+    }
+    if(!data.author){
+      error = true
+      missing.push("author")
+    }
+    if(error){
+      res.render('new_book' , {
+        "error": true,
+        "fields": missing
+      })
+    }else{
+      Book.
+      create(data)
+      .then(book=>{
+        res.redirect('/')
+      })
+      .catch(err=>{
+        console.log(err)
+      })
+    }
+  })
+
+
+  app.post('/books/:id' , (req ,res)=>{
+    let error = false;
+    let missing = [];
+    data = req.body
+    if (!data.title){
+      error = true
+      missing.push("title")
+    }
+    if(!data.author){
+      error = true
+      missing.push("author")
+    }
+    if(error){
+      res.render('book_detail' , {
+        "error": true,
+        "fields": missing,
+        ...data
+      })
+    }else{
+      Book.
+      update(data , {
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(book=>{
+        res.redirect('/')
+      })
+      .catch(err=>{
+        console.log(err)
+      })
+    }
   })
 
   app.get('/*' , (req , res)=>{
@@ -50,6 +125,3 @@ db
   app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}!`);
   });
-  // Rather than hardcoding any information inside the pug templates I have added all the information here and then I used this JSON to populate the
-  // pug varaibles in the templates. I also had to slightly modify the pug templtaes for this.
-  //Similarly, to changes any images, one can simply replace the relevant images in the images folder.
